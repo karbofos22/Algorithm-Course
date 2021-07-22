9c8da717b4cff489a5d5f9fddf59285774c3870a
Sergey <karbofos22@yandex.ru>
2021-07-23 00:23:40 +0300

    added lesso5_homework files

---
+++ Lesson5_HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new();
+
+            tree.AddItem(22);
+            tree.AddItem(45);
+            tree.AddItem(4);
+            tree.AddItem(99);
+            tree.AddItem(6);
+            tree.AddItem(1);
+            tree.AddItem(33);
+
+
+
+            Console.WriteLine("Дерево элементов");
+            tree.Print(tree.GetRoot(), 0, 2);
+            Console.WriteLine("\n");
+
+            var root = tree.GetRoot();
+            Console.WriteLine("BFS");
+            tree.QueueSearch(root, 6);
+            Console.WriteLine();
+            Console.WriteLine("BFS");
+            tree.QueueSearch(root, 13);
+            Console.WriteLine();
+            Console.WriteLine("DFS");
+            tree.StackSearch(root, 99);
+            Console.WriteLine();
+            Console.WriteLine("DFS");
+            tree.StackSearch(root, 13);
+        }
+    }
+    
+}
+
---
+++ Lesson5_HomeWork/Tree.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class Tree : ITree
+    {
+        TreeNode root;
+
+        public void AddItem(int value)
+        {
+            TreeNode newNode = new TreeNode { Value = value };
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                TreeNode currentNode = root;
+                TreeNode parent;
+
+                while (true)
+                {
+                    parent = currentNode;
+                    if (value < currentNode.Value)
+                    {
+                        currentNode = currentNode.LeftChild;
+                        if (currentNode == null)
+                        {
+                            parent.LeftChild = newNode;
+                            newNode.Parent = parent;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        currentNode = currentNode.RightChild;
+                        if (currentNode == null)
+                        {
+                            parent.RightChild = newNode;
+                            newNode.Parent = parent;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        public TreeNode GetRoot()
+        {
+            return root;
+        }
+
+        public int Print(TreeNode node, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(node.Value);
+
+            var loc = y;
+
+            if (node.RightChild != null)
+            {
+                Console.SetCursorPosition(x + 2, y);
+                Console.Write("--");
+                y = Print(node.RightChild, x + 4, y);
+            }
+
+            if (node.LeftChild != null)
+            {
+                while (loc <= y)
+                {
+                    Console.SetCursorPosition(x, loc + 1);
+                    Console.Write(" |");
+                    loc++;
+                }
+                y = Print(node.LeftChild, x, y + 2);
+            }
+            return y;
+        }
+
+        public TreeNode QueueSearch(TreeNode node, int numToFind)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            Queue<TreeNode> testQueue = new();
+            TreeNode nodeToFind = new();
+            testQueue.Enqueue(node);
+            Console.Write(node.Value+" ");
+            while (testQueue.Count > 0)
+            {
+                nodeToFind = testQueue.Dequeue();
+                
+                if (nodeToFind.Value == numToFind)
+                {
+                    Console.WriteLine($"\nNode {numToFind} was found");
+                    return nodeToFind;
+                }
+                else
+                {
+                    if (nodeToFind.LeftChild != null)
+                    {
+                        testQueue.Enqueue(nodeToFind.LeftChild);
+                        Console.Write(nodeToFind.LeftChild.Value + " ");
+                    }
+                    if (nodeToFind.RightChild != null)
+                    {
+                        testQueue.Enqueue(nodeToFind.RightChild);
+                        Console.Write(nodeToFind.RightChild.Value + " ");
+                    }
+                }
+            }
+            Console.WriteLine($"\nNode {numToFind} not Found");
+            return null;
+        }
+        public TreeNode StackSearch(TreeNode node, int numToFind)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            Stack<TreeNode> testStack = new();
+            TreeNode currentNode = node;
+
+            while (currentNode != null || testStack.Count != 0)
+            {
+                while (currentNode != null)
+                {
+                    Console.Write(currentNode.Value + " ");
+                    if (currentNode.Value == numToFind)
+                    {
+                        Console.WriteLine($"\nNode {numToFind} was found");
+                        return currentNode;
+                    }
+                    if (currentNode.RightChild != null)
+                        testStack.Push(currentNode.RightChild);
+
+                    currentNode = currentNode.LeftChild;
+                }
+                if (testStack.Count != 0)
+                {
+                    currentNode = testStack.Pop();
+                }
+            }
+            Console.WriteLine($"\nNode {numToFind} not Found");
+            return null;
+        }
+    }
+}
