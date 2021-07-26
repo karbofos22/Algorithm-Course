7462b51ba2f88c3d19597c2dd6df8fe50729adfa
Sergey <karbofos22@yandex.ru>
2021-07-26 22:45:36 +0300

    added some..... thing.. for Lesson6_HomeWork

---
+++ Lesson6_HomeWork/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_HomeWork
+{
+    public class Node //Вершина
+    {
+        public string Name { get; set; }
+        public override string ToString() => Name;
+        public List<Edge> Edges { get; set; } //исходящие связи
+
+        public Node(string name)
+        {
+            this.Name = name;
+        }
+
+
+        public void PrintConnections(List<Edge> Edges)
+        {
+            foreach (var item in Edges)
+            {
+                Console.WriteLine($"{Name} reffers to {item.Node.Name}, weight {item.Weight}");
+            }
+        }
+    }
+
+    public class Edge //Ребро
+    {
+       
+        public int Weight { get; set; } //вес связи
+        public Node Node { get; set; } // узел, на который связь ссылается
+        public override string ToString() => Node.Name;
+
+        public Edge(Node node, int weight)
+        {
+            this.Node = node;
+            this.Weight = weight;
+        }
+    }
+}
