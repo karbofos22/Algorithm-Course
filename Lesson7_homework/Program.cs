672b393be1225db20a29e2996c90f4549abcac15
Sergey <karbofos22@yandex.ru>
2021-07-31 17:40:03 +0300

    added some code....

--- Lesson7_homework/Program.cs
+++ Lesson7_homework/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace Lesson7_homework
 {
@@ -6,39 +8,40 @@ namespace Lesson7_homework
     {
         static void Main(string[] args)
         {
-            //            1.Задача
+            //1.Задача
             //Для прямоугольного поля размера M на N клеток, подсчитать количество путей из верхней левой клетки в правую нижнюю.
             //Известно что ходить можно только на одну клетку вправо или вниз.
 
             int[,] test = new int[3, 6];
             int i, j;
+            int ways = 0;
+            
             for (j = 0; j < test.GetLength(1); j++)
-                test[0, j] = 1; // Первая строка заполнена единицами
+                test[0, j] = 1; 
             for (i = 1; i < test.GetLength(0); i++)
             {
                 test[i, 0] = 1;
+                
                 for (j = 1; j < test.GetLength(1); j++)
                 {
-                    if (test[i])
-                    {
-
-                    }
                     test[i, j] = test[i, j - 1] + test[i - 1, j];
                 }
-                  
+            }
+            foreach (int item in test)
+            {
+                ways = ways < item ? item : ways;
             }
 
-            Print2(test);
-
-
+            PrintArray(test);
+            Console.WriteLine($"Кол-во маршрутов из верхней левой клетки в правую нижнюю равно {ways}");
         }
-        static void Print2(int[,] a)
+        static void PrintArray(int[,] num)
         {
             int i, j;
-            for (i = 0; i < a.GetLength(0); i++)
+            for (i = 0; i < num.GetLength(0); i++)
             {
-                for (j = 0; j < a.GetLength(1); j++)
-                    Console.Write(a[i, j] + "\t");
+                for (j = 0; j < num.GetLength(1); j++)
+                    Console.Write(num[i, j] + "\t");
                 Console.Write("\r\n");
             }
         }
